6adec33d1f654cd308386529cc40382a165b697f
John Mark Redding <johnmarkredding@gmail.com>
2018-11-11 02:04:06 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,7 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each {|x,i|
-    puts "#{x}"
+    puts "Happy Birthday #{x}! You are now #{i} years old!"
   }
 end
 
